45516822ef7e1908c5b602d7cf4e8313749223a5
Artem Kutuzov <electro-mpo-it@yandex.ru>
2024-06-14 15:00:43 +0300

    Add tags field to product msg

--- gen/py/products/products_pb2.pyi
+++ gen/py/products/products_pb2.pyi
@@ -403,7 +403,7 @@ class AvailableFilters(_message.Message):
     def __init__(self, price: _Optional[_Union[Range, _Mapping]] = ..., characteristics: _Optional[_Union[CharacteristicsFilter, _Mapping]] = ...) -> None: ...
 
 class Product(_message.Message):
-    __slots__ = ("id", "name", "category_id", "description", "unit_of_measurement", "vat", "old_price", "discount_ratio", "price", "is_visible", "updated_at", "images", "created_at", "product_characteristics")
+    __slots__ = ("id", "name", "category_id", "description", "unit_of_measurement", "vat", "old_price", "discount_ratio", "price", "is_visible", "updated_at", "images", "created_at", "product_characteristics", "tags")
     ID_FIELD_NUMBER: _ClassVar[int]
     NAME_FIELD_NUMBER: _ClassVar[int]
     CATEGORY_ID_FIELD_NUMBER: _ClassVar[int]
@@ -418,6 +418,7 @@ class Product(_message.Message):
     IMAGES_FIELD_NUMBER: _ClassVar[int]
     CREATED_AT_FIELD_NUMBER: _ClassVar[int]
     PRODUCT_CHARACTERISTICS_FIELD_NUMBER: _ClassVar[int]
+    TAGS_FIELD_NUMBER: _ClassVar[int]
     id: str
     name: str
     category_id: str
@@ -432,4 +433,5 @@ class Product(_message.Message):
     images: _containers.RepeatedCompositeFieldContainer[ProductImage]
     created_at: _timestamp_pb2.Timestamp
     product_characteristics: _containers.RepeatedCompositeFieldContainer[ProductCharacteristic]
-    def __init__(self, id: _Optional[str] = ..., name: _Optional[str] = ..., category_id: _Optional[str] = ..., description: _Optional[str] = ..., unit_of_measurement: _Optional[_Union[UnitOfMeasurementENUM, str]] = ..., vat: _Optional[_Union[VATENUM, str]] = ..., old_price: _Optional[float] = ..., discount_ratio: _Optional[float] = ..., price: _Optional[float] = ..., is_visible: bool = ..., updated_at: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., images: _Optional[_Iterable[_Union[ProductImage, _Mapping]]] = ..., created_at: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., product_characteristics: _Optional[_Iterable[_Union[ProductCharacteristic, _Mapping]]] = ...) -> None: ...
+    tags: _containers.RepeatedCompositeFieldContainer[Tag]
+    def __init__(self, id: _Optional[str] = ..., name: _Optional[str] = ..., category_id: _Optional[str] = ..., description: _Optional[str] = ..., unit_of_measurement: _Optional[_Union[UnitOfMeasurementENUM, str]] = ..., vat: _Optional[_Union[VATENUM, str]] = ..., old_price: _Optional[float] = ..., discount_ratio: _Optional[float] = ..., price: _Optional[float] = ..., is_visible: bool = ..., updated_at: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., images: _Optional[_Iterable[_Union[ProductImage, _Mapping]]] = ..., created_at: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ..., product_characteristics: _Optional[_Iterable[_Union[ProductCharacteristic, _Mapping]]] = ..., tags: _Optional[_Iterable[_Union[Tag, _Mapping]]] = ...) -> None: ...
